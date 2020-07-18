cf9a599adf065cdb18cce5d6294f4c1af5272990
danilobastos <danilobastos2@gmail.com>
2020-07-18 15:18:50 -0300

    adding simple condictional structure

---
+++ condictional_structure/simple_condictional.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3.8
+
+# Example code for condictional structure
+
+# first simple structure
+a = 1
+
+if a == 1:
+    print("a is equal one")
+
+# second simple structure
+b = 2
+
+if b != 2:
+    print("b is not equal two")
+else:
+    print("b is equal two")
+
+
