b395a9393d0682d6ed88d3c3dc072aa484aa4735
danilobastos <danilobastos2@gmail.com>
2020-07-18 15:06:25 -0300

    adding variables and your types

---
+++ variables/variables_types.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3.8
+
+# simple variable (type string)
+a = 'Dog'
+print("Variable " + a + " is type string")
+
+# validate if a is type string
+print(type(a))
+
+# simple variable (type integer)
+b = 10
+print ("Variable " + str(b) + " is type integer")
+
+# validate if b is type integer
+print(type(b))
+
+# simple variable (type float)
+c = 3.14
+print ("Variable " + str(c) + " is value of pi on float")
+
+# validate if c is type float
+print(type(c))
+
+
